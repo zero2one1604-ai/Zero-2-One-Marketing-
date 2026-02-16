b630288323bc59bcdea777e51bd11d3de23f80ba
Pravin <pravingupta2020@gmail.com>
2026-02-17 00:09:42 +0530

    `Added Facebook pixel script and noscript tag to layout.js`

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "@/app/components/Navbar";
 import localFont from "next/font/local";
 import { AuthModalProvider } from "@/app/components/AuthModalProvider";
 import ClientReveal from "./components/ClientReveal";
+import Script from "next/script";
 
 const theSeasons = localFont({
   src: "../../public/fonts/theseasons.otf",
@@ -81,12 +82,39 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={theSeasons.variable}>
       <body>
+        <noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: 'none' }}
+    src="https://www.facebook.com/tr?id=25606478475681905&ev=PageView&noscript=1"
+  />
+</noscript>
+         <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '25606478475681905');
+            fbq('track', 'PageView');
+          `}
+        </Script>
    <AuthModalProvider>
         <ClientReveal>
             <Navbar />
             {children}
           </ClientReveal>
         </AuthModalProvider>
+        
       </body>
     </html>
   );
